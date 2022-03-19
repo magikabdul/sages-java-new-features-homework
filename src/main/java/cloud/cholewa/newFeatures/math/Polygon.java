3cddbcbfe2f84bb6e4f5e362b675ea08aa056975
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-03-19 21:47:13 +0100

    added abstract class Polygon in string and math version

---
+++ src/main/java/cloud/cholewa/newFeatures/math/Polygon.java
@@ -0,0 +1,9 @@
+package cloud.cholewa.newFeatures.math;
+
+public abstract class Polygon {
+    private int numberOfVertices;
+
+    public int getNumberOfVertices() {
+        return numberOfVertices;
+    }
+}
---
+++ src/main/java/cloud/cholewa/newFeatures/strings/Polygon.java
@@ -0,0 +1,9 @@
+package cloud.cholewa.newFeatures.strings;
+
+public abstract class Polygon {
+    private int numberOfVertices;
+
+    public int getNumberOfVertices() {
+        return numberOfVertices;
+    }
+}
